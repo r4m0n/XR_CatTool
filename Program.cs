260c6d53da84ffe418a9e69a1037356130be8b36
Timothy <timothyparez@gmail.com>
2013-11-25 23:55:23 +0800

    Added some input validation
    Added basic argument input validation
    and some minor refactoring

--- Program.cs
+++ Program.cs
@@ -8,44 +8,84 @@ namespace XCatTool
 {
     class Program
     {
-        const int BUFFSIZE = 1024 * 64;
+        const int BUFFER_SIZE = 1024 * 64;
 
         static void Main(string[] args)
         {
-            var catFileContents = File.ReadAllLines(args[0] + ".cat");
-            var datFileStream = File.OpenRead(args[0] + ".dat");
-            var buffer = new byte[BUFFSIZE];
+            new Program(args);
+        }
 
-            foreach (var line in catFileContents)
+        public Program(string[] args)
+        {
+            if (args.Length != 0)
             {
-                var pieces = line.Split(' ').ToList();
-                if (pieces.Count >= 4)
+                Console.WriteLine("Usage: XCatTool.exe <catfilename>");
+                return;
+            }
+
+            var inputFile = Path.GetFileNameWithoutExtension(args[0]);
+            var catFile = String.Concat(inputFile, ".cat");
+            var datFile = String.Concat(inputFile, ".dat");
+
+            if (!File.Exists(catFile) || !File.Exists(datFile))
+            {
+                Console.WriteLine("Could not locate the {0} or {1} files", catFile, datFile);
+                return;
+            }
+
+            Extract(catFile, datFile);
+        }
+
+        private void Extract(string catFile, string datFile)
+        {
+            var catFileContents = File.ReadAllLines(catFile);
+            using (var datFileStream = File.OpenRead(datFile))
+            {
+                var buffer = new byte[BUFFER_SIZE];
+
+                foreach (var line in catFileContents)
                 {
-                    while (pieces.Count > 4)
+                    var pieces = line.Split(' ').ToList();
+                    if (pieces.Count >= 4)
                     {
-                        pieces[0] = pieces[0] + pieces[1];
-                        pieces.RemoveAt(1);
+                        while (pieces.Count > 4)
+                        {
+                            pieces[0] = pieces[0] + pieces[1];
+                            pieces.RemoveAt(1);
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(Path.GetDirectoryName(pieces[0])) && !Directory.Exists(Path.GetDirectoryName(pieces[0])))
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(pieces[0]));
+                        }
+
+                        using (var outputFile = File.OpenWrite(pieces[0]))
+                        {
+                            int remBytes = int.Parse(pieces[1]);
+
+                            while (remBytes > 0)
+                            {
+                                int br = datFileStream.Read(buffer, 0, Math.Min(BUFFER_SIZE, remBytes));
+                                outputFile.Write(buffer, 0, br);
+                                remBytes -= br;
+                            }
+                        }                        
                     }
-                    if (!string.IsNullOrWhiteSpace(Path.GetDirectoryName(pieces[0])) && !Directory.Exists(Path.GetDirectoryName(pieces[0])))
+                    else
                     {
-                        Directory.CreateDirectory(Path.GetDirectoryName(pieces[0]));
+                        Console.WriteLine("Invalid line, contents: " + line);
                     }
-                    FileStream of = File.OpenWrite(pieces[0]);
-                    int remBytes = int.Parse(pieces[1]);
-                    while (remBytes > 0)
-                    {
-                        int br = datFileStream.Read(buffer, 0, Math.Min(BUFFSIZE, remBytes));
-                        of.Write(buffer, 0, br);
-                        remBytes -= br;
-                    }
-                    of.Close();
-                }
-                else
-                {
-                    Console.WriteLine("Invalid line, contents: " + line);
                 }
             }
-            datFileStream.Close();
+        }
+
+        private string GetInputFilename(string[] args)
+        {
+            var filename = args[0];
+            if (filename.EndsWith(".cat") || filename.EndsWith(".dat"))
+                filename = filename.Substring(0, filename.Length - 4);
+
+            return filename;
         }
     }
 }
